9cd07054a60fc428886cab65baf0b97d38d690ac
VivekBcloud <20vivektoppo@gmail.com>
2022-01-08 23:28:30 +0530

    added the agency page

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,7 +1,108 @@
 import React from "react";
-
+import aboutstyles from "../styles/about.module.css";
+import styles from "../styles/agency.module.css";
 const Agency = () => {
-  return <div>Agency page</div>;
+  return (
+    <div className={`${aboutstyles.container}  fluid-container`}>
+      <div className={aboutstyles.headerSection}>
+        <h1>Agency Request</h1>
+        <img src="/svg/wave.svg" alt="wave" className={aboutstyles.waveImg} />
+      </div>
+      <div className={styles.innerContainer}>
+        <div className={styles.agencyTitle}>Agency Request</div>
+        <form className={styles.formContainer}>
+          <div>
+            <label htmlFor="name">
+              Agency Name <span>*</span>{" "}
+            </label>
+            <input type="text" id="name" required placeholder="Agency Name" />
+          </div>
+          <div>
+            <label htmlFor="website">Agency Website</label>
+            <input type="text" id="website" placeholder="Agency Website" />
+          </div>
+          <div>
+            <label htmlFor="contactName">
+              Contact Person Name <span>*</span>{" "}
+            </label>
+            <input
+              type="text"
+              id="contactName"
+              required
+              placeholder="Contact Person Name "
+            />
+          </div>
+          <div>
+            <label htmlFor="designation">
+              Contact Person Designation<span>*</span>{" "}
+            </label>
+            <input
+              type="text"
+              id="designation"
+              required
+              placeholder="Contact Person Designation "
+            />
+          </div>
+          <div>
+            <label htmlFor="mobile">
+              Mobile Number <span>*</span>{" "}
+            </label>
+            <input
+              type="number"
+              id="mobile"
+              required
+              placeholder="Mobile Number"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">
+              Your Email <span>*</span>{" "}
+            </label>
+            <input type="email" id="email" required placeholder="Your Email" />
+          </div>
+          <div>
+            <label htmlFor="country">
+              Country <span>*</span>{" "}
+            </label>
+            <input type="text" id="country" required placeholder="Country" />
+          </div>
+          <div>
+            <label htmlFor="state">
+              State <span>*</span>{" "}
+            </label>
+            <input type="text" id="state" required placeholder="State" />
+          </div>
+          <div>
+            <label htmlFor="streamer">
+              No. of Streamers <span>*</span>{" "}
+            </label>
+            <input
+              type="text"
+              id="streamer"
+              required
+              placeholder="No. of Streamers"
+            />
+          </div>
+          <div>
+            <label htmlFor="experience">
+              Work Experience <span>*</span>{" "}
+            </label>
+            <input
+              type="text"
+              id="experience"
+              required
+              placeholder="Work Experience"
+            />
+          </div>
+          <div>
+            <label htmlFor="referred">Referred By(Optional) </label>
+            <input type="text" id="referred" placeholder="Referred By" />
+          </div>
+          <button className={styles.submitBtn}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Agency;
